8fdf7d07479691bcb564aea533dfbdaf7b54f3d6
ramoncord <ramonestii@hotmail.com>
2018-06-21 23:05:36 -0300

    editando exchange-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'exchanges/index'
-  get 'exchanges/convert'
+  root 'exchanges#index'
+  get 'convert', to: 'exchanges#convert'
+  #get 'exchanges/index'
+  #get 'exchanges/convert'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
