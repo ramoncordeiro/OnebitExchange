66c30461b89749d8808190231237c1fb8b2208a8
ramoncord <ramonestii@hotmail.com>
2018-06-21 23:33:45 -0300

    Testes de Sistema

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,11 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+  
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
